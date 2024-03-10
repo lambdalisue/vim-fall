ab22352baadf940684ae19d14c54ee448d5895b6
Alisue <lambdalisue@gmail.com>
2024-03-10 16:49:32 +0900

    :shower: Remove `github:` resolver

--- denops/fall/extension/resolver.ts
+++ denops/fall/extension/resolver.ts
@@ -1,10 +1,9 @@
 import type { ExtensionConfig } from "../config/extension.ts";
 import { resolve as builtin } from "./resolver/builtin.ts";
-import { resolve as github } from "./resolver/github.ts";
 
 export async function resolve<K extends keyof ExtensionConfig>(
   kind: K,
   uri: string,
 ): Promise<URL> {
-  return await github(kind, uri) || await builtin(kind, uri) || new URL(uri);
+  return await builtin(kind, uri) || new URL(uri);
 }
