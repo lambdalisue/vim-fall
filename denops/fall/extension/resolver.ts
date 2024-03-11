987cf218ee1a5c7b06989fe24ed01d0cdf6de0d2
Alisue <lambdalisue@gmail.com>
2024-03-12 07:58:41 +0900

    :shower: Remove `package` resolver

--- denops/fall/extension/resolver.ts
+++ denops/fall/extension/resolver.ts
@@ -1,13 +1,11 @@
 import { resolve as resolveBuiltin } from "./resolver/builtin.ts";
 import { resolve as resolveGitHub } from "./resolver/github.ts";
-import { resolve as resolvePackage } from "./resolver/package.ts";
 
 export async function resolve(
   uri: string,
   base?: URL | string,
 ): Promise<URL> {
   return await resolveBuiltin(uri) ||
-    await resolvePackage(uri) ||
     await resolveGitHub(uri) ||
     new URL(uri, base);
 }
