3f0575577802241164cb0b8788b25cd9806acc85
Alisue <lambdalisue@gmail.com>
2024-03-12 10:04:33 +0900

    :shower: Remove github resolver

--- denops/fall/extension/resolver.ts
+++ denops/fall/extension/resolver.ts
@@ -1,11 +1,8 @@
 import { resolve as resolveBuiltin } from "./resolver/builtin.ts";
-import { resolve as resolveGitHub } from "./resolver/github.ts";
 
 export async function resolve(
   url: string,
   base?: URL | string,
 ): Promise<URL> {
-  return await resolveBuiltin(url) ||
-    await resolveGitHub(url) ||
-    new URL(url, base);
+  return await resolveBuiltin(url) || new URL(url, base);
 }
