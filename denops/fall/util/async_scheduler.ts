c38da8da140a0002f4cd2108ff1201b7c60333de
Alisue <lambdalisue@gmail.com>
2024-03-16 16:56:19 +0900

    :muscle: Refine `startAsyncScheduler` function

--- denops/fall/util/async_scheduler.ts
+++ denops/fall/util/async_scheduler.ts
@@ -1,67 +1,50 @@
 /**
- * Asynchronous scheduler
+ * Start asynchronous scheduler
  *
- * It is used to schedule asynchronous tasks at regular intervals.
- */
-export class AsyncScheduler implements Disposable {
-  #timer?: number;
-  #callback: () => Promise<void>;
-  #interval: number;
-
-  constructor(
-    callback: () => Promise<void>,
-    interval: number,
-  ) {
-    this.#callback = callback;
-    this.#interval = interval;
-  }
-
-  /**
-   * Start the scheduler
-   */
-  start({ signal }: { signal?: AbortSignal } = {}): void {
-    if (signal?.aborted) return;
-    signal?.addEventListener("abort", () => {
-      this.#stop();
-    });
-    const start = () => {
-      this.#timer = setTimeout(async () => {
-        if (signal?.aborted) return;
-        try {
-          await this.#callback();
-          start();
-        } catch (err) {
-          // Avoid unhandled rejection
-          console.error(
-            `[fall] Unexpected error occured in asyncrhonous scheduler: ${err}`,
-          );
-        }
-      }, this.#interval);
-    };
-    start();
-  }
-
-  #stop(): void {
-    if (this.#timer != undefined) {
-      clearTimeout(this.#timer);
-      this.#timer = undefined;
-    }
-  }
-
-  [Symbol.dispose]() {
-    this.#stop();
-  }
-}
-
-/**
- * Start an asynchronous scheduler
+ * It periodically calls the given asynchronous callback function
+ * with the given interval without overlapping.
+ * It means that the next call is scheduled after the previous call
+ * is finished.
+ *
+ * The scheduler stops when the callback throws an error or returned
+ * disposable is disposed.
  */
 export function startAsyncScheduler(
   callback: () => Promise<void>,
   interval: number,
   { signal }: { signal?: AbortSignal } = {},
 ): Disposable {
-  const scheduler = new AsyncScheduler(callback, interval);
-  scheduler.start({ signal });
-  return scheduler;
+  if (signal?.aborted) {
+    return {
+      [Symbol.dispose]() {},
+    };
+  }
+  let timer: number | undefined;
+  const stop = () => {
+    if (timer != undefined) {
+      clearTimeout(timer);
+      timer = undefined;
+    }
+  };
+  signal?.addEventListener("abort", stop);
+  const start = () => {
+    timer = setTimeout(async () => {
+      if (signal?.aborted) return;
+      try {
+        await callback();
+        start();
+      } catch (err) {
+        if (err instanceof DOMException && err.name === "AbortError") return;
+        console.error(
+          `[fall] Unexpected error occured in asyncrhonous scheduler: ${err}`,
+        );
+      }
+    }, interval);
+  };
+  start();
+  return {
+    [Symbol.dispose]() {
+      stop();
+    },
+  };
 }
--- denops/fall/util/async_scheduler_test.ts
+++ denops/fall/util/async_scheduler_test.ts
@@ -1,50 +1,112 @@
+import { delay } from "https://deno.land/std@0.218.2/async/mod.ts";
 import { assertEquals } from "https://deno.land/std@0.218.2/assert/mod.ts";
-import { AsyncScheduler, startAsyncScheduler } from "./async_scheduler.ts";
+import { startAsyncScheduler } from "./async_scheduler.ts";
 
-Deno.test("AsyncScheduler - start and stop", async () => {
-  let counter = 0;
+Deno.test("startAsyncScheduler", async (t) => {
+  await t.step(
+    "calls the given asynchronous callback function with the given interval without overlapping",
+    async () => {
+      const controller = new AbortController();
+      const { signal } = controller;
+      let count = 0;
+      let locked = false;
+      {
+        using _disposable = startAsyncScheduler(async () => {
+          if (locked) {
+            throw new Error("Overlapping");
+          }
+          locked = true;
+          await delay(100, { signal });
+          count++;
+          locked = false;
+        }, 100);
+        await delay(900);
+      }
+      controller.abort();
+      // ```
+      // Count             1                   2                   3                   4
+      // -------->=========|--------->=========|--------->=========|--------->=========|---------x
+      // Time    100       200       300       400       500       600       700       800       900
+      // ```
+      assertEquals(count, 4);
+    },
+  );
 
-  // deno-lint-ignore require-await
-  const callback = async () => {
-    counter++;
-  };
+  await t.step(
+    "stops when the callback throws an error",
+    async () => {
+      let count = 0;
+      {
+        using _disposable = startAsyncScheduler(
+          () => {
+            count++;
+            throw new Error("Error");
+          },
+          100,
+        );
+        await delay(500);
+      }
+      // ```
+      // Count   1
+      // --------x........................................
+      // Time    100       200       300       400       500
+      // ```
+      assertEquals(count, 1);
+    },
+  );
 
-  const interval = 100; // milliseconds
-  using scheduler = new AsyncScheduler(callback, interval);
+  await t.step(
+    "does not call the given asynchronous callback function if the signal is already aborted",
+    async () => {
+      const controller = new AbortController();
+      const { signal } = controller;
+      controller.abort();
+      let count = 0;
+      {
+        using _disposable = startAsyncScheduler(
+          () => {
+            count++;
+            return Promise.resolve();
+          },
+          100,
+          { signal },
+        );
+        await delay(200);
+      }
+      // ```
+      // Count
+      // ...................
+      // Time    100       200
+      // ```
+      assertEquals(count, 0);
+    },
+  );
 
-  assertEquals(counter, 0);
-
-  scheduler.start();
-  await new Promise((resolve) => setTimeout(resolve, interval * 3)); // Wait for a few intervals
-
-  scheduler[Symbol.dispose]();
-
-  const initialCounter = counter;
-
-  // Ensure that the counter hasn't changed after stopping
-  await new Promise((resolve) => setTimeout(resolve, interval * 3));
-  assertEquals(counter, initialCounter);
-});
-
-Deno.test("startAsyncScheduler - start and stop", async () => {
-  let counter = 0;
-
-  // deno-lint-ignore require-await
-  const callback = async () => {
-    counter++;
-  };
-
-  const interval = 100; // milliseconds
-  using disposable = startAsyncScheduler(callback, interval);
-
-  assertEquals(counter, 0);
-
-  await new Promise((resolve) => setTimeout(resolve, interval * 3)); // Wait for a few intervals
-
-  disposable[Symbol.dispose]();
-  const initialCounter = counter;
-
-  // Ensure that the counter hasn't changed after stopping
-  await new Promise((resolve) => setTimeout(resolve, interval * 3));
-  assertEquals(counter, initialCounter);
+  await t.step(
+    "does not call the given asynchronous callback function after the signal is aborted",
+    async () => {
+      const controller = new AbortController();
+      const { signal } = controller;
+      let count = 0;
+      {
+        using _disposable = startAsyncScheduler(
+          () => {
+            count++;
+            return Promise.resolve();
+          },
+          100,
+          { signal },
+        );
+        await delay(250);
+        controller.abort();
+        await delay(250);
+      }
+      // ```
+      // Count   1         2
+      // --------|---------|----x.........................
+      // Time    100       200       300       400       500
+      // ```
+      assertEquals(count, 2);
+    },
+  );
 });
