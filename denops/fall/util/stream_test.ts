266ad2e9300e07efeac9a9fdf77a27c826d70f27
Alisue <lambdalisue@gmail.com>
2024-03-16 16:58:20 +0900

    :muscle: Refine `ChunkStream` tests

--- denops/fall/util/stream_test.ts
+++ denops/fall/util/stream_test.ts
@@ -14,4 +14,30 @@ Deno.test("ChunkStream", async (t) => {
     );
     assertEquals(chunks, [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]);
   });
+
+  await t.step("works with stream with smaller size", async () => {
+    const chunks: number[][] = [];
+    const rstream = ReadableStream.from([...Array(10).keys()]);
+    await rstream.pipeThrough(new stream.ChunkStream(20)).pipeTo(
+      new WritableStream({
+        write(chunk) {
+          chunks.push(chunk);
+        },
+      }),
+    );
+    assertEquals(chunks, [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]);
+  });
+
+  await t.step("works with an empty stream", async () => {
+    const chunks: number[][] = [];
+    const rstream = ReadableStream.from([]);
+    await rstream.pipeThrough(new stream.ChunkStream(3)).pipeTo(
+      new WritableStream({
+        write(chunk) {
+          chunks.push(chunk);
+        },
+      }),
+    );
+    assertEquals(chunks, []);
+  });
 });
