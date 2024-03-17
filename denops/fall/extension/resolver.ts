4546a0f71d929fe869f843307186dffcfae851e4
Alisue <lambdalisue@gmail.com>
2024-03-17 16:31:19 +0900

    :muscle: Refine `resolver` module

--- denops/fall/extension/resolver.ts
+++ denops/fall/extension/resolver.ts
@@ -1,8 +1,21 @@
-import { resolve as resolveBuiltin } from "./resolver/builtin.ts";
+import { match, placeholder as _ } from "jsr:@core/match@0.2.5";
+
+const builtinPattern = _`fallbuiltin://${_("path")}`;
 
 export async function resolve(
   url: string,
   base?: URL | string,
 ): Promise<URL> {
   return await resolveBuiltin(url) || new URL(url, base);
+}
+
+function resolveBuiltin(
+  url: string,
+): Promise<URL | undefined> {
+  const m = match(builtinPattern, url);
+  if (!m) return Promise.resolve(undefined);
+  const { path } = m;
+  return Promise.resolve(
+    new URL(`../../@fall-builtin/${path}`, import.meta.url),
+  );
 }
