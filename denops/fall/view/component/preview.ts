4fcd15f3aed834699bfe461449022b240e19f49d
Alisue <lambdalisue@gmail.com>
2024-05-19 13:35:59 +0900

    :muscle: Simplify `PreviewComponent`

--- denops/fall/view/component/preview.ts
+++ denops/fall/view/component/preview.ts
@@ -1,19 +1,16 @@
 import type { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
-import {
-  batch,
-  collect,
-} from "https://deno.land/x/denops_std@v6.4.0/batch/mod.ts";
+import { collect } from "https://deno.land/x/denops_std@v6.4.0/batch/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
 import * as buffer from "https://deno.land/x/denops_std@v6.4.0/buffer/mod.ts";
 
 import type { Previewer, PreviewerItem } from "../../extension/type.ts";
 
-export type PreviewComponentParams = Readonly<{
+export type Params = Readonly<{
   previewers?: readonly Previewer[];
 }>;
 
 /**
- * A component that renders a preview window.
+ * Preview component that shows preview content of the cursor item
  */
 export class PreviewComponent {
   #bufnr: number;
@@ -23,77 +20,67 @@ export class PreviewComponent {
   constructor(
     bufnr: number,
     winid: number,
-    params: PreviewComponentParams,
+    params: Params,
   ) {
     this.#bufnr = bufnr;
     this.#winid = winid;
     this.#previewers = params.previewers ?? [];
   }
 
-  /**
-   * Render the preview window.
-   *
-   * It returns true if the preview window is rendered.
-   */
   async render(
     denops: Denops,
     item: PreviewerItem | undefined,
-    options: { signal: AbortSignal },
+    { signal }: { signal: AbortSignal },
   ): Promise<void> {
     try {
-      await this.#render(denops, item, options);
+      // Overwrite buffer local options may configured by ftplugin
+      await fn.win_execute(
+        denops,
+        this.#winid,
+        `setlocal buftype=nofile bufhidden=wipe nobuflisted noswapfile cursorline nomodifiable nowrap`,
+      );
+      signal.throwIfAborted();
+
+      if (!item) {
+        await buffer.replace(denops, this.#bufnr, [
+          "No preview item is available",
+        ]);
+        return;
+      }
+
+      const params = {
+        item,
+        bufnr: this.#bufnr,
+        winid: this.#winid,
+      };
+      let previewed = false;
+      for (const previewer of this.#previewers) {
+        if (await previewer.preview(params, { signal })) {
+          signal.throwIfAborted();
+          continue;
+        }
+        previewed = true;
+        break;
+      }
+      signal.throwIfAborted();
+
+      if (previewed) {
+        await fn.win_execute(
+          denops,
+          this.#winid,
+          `silent! filetype detect`,
+        );
+      } else {
+        await buffer.replace(denops, this.#bufnr, [
+          "No preview is available",
+        ]);
+      }
     } catch (err) {
       if (err instanceof DOMException && err.name === "AbortError") return;
       const m = err.message ?? err;
       console.warn(
         `[fall] Failed to render the preview component: ${m}`,
       );
-    }
-  }
-
-  async #render(
-    denops: Denops,
-    item: PreviewerItem | undefined,
-    { signal }: { signal: AbortSignal },
-  ): Promise<void> {
-    // Overwrite buffer local options may configured by ftplugin
-    await fn.win_execute(
-      denops,
-      this.#winid,
-      `setlocal buftype=nofile bufhidden=wipe nobuflisted noswapfile cursorline nomodifiable nowrap`,
-    );
-    signal.throwIfAborted();
-
-    if (!item) {
-      await buffer.replace(denops, this.#bufnr, [
-        "No preview item is available",
-      ]);
-      return;
-    }
-
-    const params = {
-      item,
-      bufnr: this.#bufnr,
-      winid: this.#winid,
-    };
-    let previewed = false;
-    for (const previewer of this.#previewers) {
-      if (await previewer.preview(params, { signal })) {
-        continue;
-      }
-      previewed = true;
-      break;
-    }
-    if (previewed) {
-      await fn.win_execute(
-        denops,
-        this.#winid,
-        `silent! filetype detect`,
-      );
-    } else {
-      await buffer.replace(denops, this.#bufnr, [
-        "No preview is available",
-      ]);
     }
   }
 
@@ -110,7 +97,7 @@ export class PreviewComponent {
         fn.line(denops, ".", this.#winid),
         fn.line(denops, "$", this.#winid),
       ]);
-      if (signal.aborted) return;
+      signal.throwIfAborted();
 
       const newLine = Math.max(1, Math.min(line + offset, linecount));
       await fn.win_execute(
@@ -119,9 +106,9 @@ export class PreviewComponent {
         `normal! ${newLine}G`,
       );
     } catch (err) {
-      // Fail silently
+      if (err instanceof DOMException && err.name === "AbortError") return;
       const m = err.message ?? err;
-      console.debug(
+      console.warn(
         `[fall] Failed to move cursor on the preview window: ${m}`,
       );
     }
@@ -137,19 +124,16 @@ export class PreviewComponent {
   ): Promise<void> {
     try {
       const linecount = await fn.line(denops, "$", this.#winid);
-      if (signal.aborted) return;
+      signal.throwIfAborted();
 
       const newLine = Math.max(1, Math.min(line, linecount));
-      await batch(denops, async (denops) => {
-        await fn.win_execute(
-          denops,
-          this.#winid,
-          `normal! ${newLine}G`,
-        );
-        await denops.cmd("redraw");
-      });
+      await fn.win_execute(
+        denops,
+        this.#winid,
+        `normal! ${newLine}G`,
+      );
     } catch (err) {
-      // Fail silently
+      if (err instanceof DOMException && err.name === "AbortError") return;
       const m = err.message ?? err;
       console.debug(
         `[fall] Failed to move cursor on the preview window: ${m}`,
