636699de544951d93df4f5e7b2f3e641f825bb1b
Alisue <lambdalisue@gmail.com>
2024-05-22 04:24:19 +0900

    :+1: Support multiple terms on `fzf`

--- denops/@fall-extension/projector/fzf.ts
+++ denops/@fall-extension/projector/fzf.ts
@@ -9,26 +9,30 @@ export const getProjector: GetProjector = (_denops, options) => {
   assert(options, isOptions);
   return {
     async project({ query, items }, { signal }) {
-      const fzf = new AsyncFzf(items, {
-        selector: (v) => v.label ?? v.value,
-      });
-      const found = await fzf.find(query);
-      signal?.throwIfAborted();
-
-      return found
-        .map((v) => {
-          const column = Math.max(0, v.start + 1);
-          const length = Math.max(0, v.end - v.start);
-          if (length === 0) return v.item;
-          const decoration: ItemDecoration = {
-            column,
-            length,
-          };
-          return {
-            ...v.item,
-            decorations: [...v.item.decorations, decoration],
-          };
+      const terms = query.split(/\s+/).filter((v) => v.length > 0);
+      let filteredItems = items;
+      for (const term of terms) {
+        const fzf = new AsyncFzf(filteredItems, {
+          selector: (v) => v.label ?? v.value,
         });
+        const found = await fzf.find(term);
+        signal?.throwIfAborted();
+        filteredItems = found
+          .map((v) => {
+            const column = Math.max(0, v.start + 1);
+            const length = Math.max(0, v.end - v.start);
+            if (length === 0) return v.item;
+            const decoration: ItemDecoration = {
+              column,
+              length,
+            };
+            return {
+              ...v.item,
+              decorations: [...v.item.decorations, decoration],
+            };
+          });
+      }
+      return filteredItems;
     },
   };
 };
