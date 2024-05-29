744cafd95be53f16a6402cc482f1f805011ced05
Alisue <lambdalisue@gmail.com>
2024-05-30 06:14:46 +0900

    :+1: Add `oldfiles` source

---
+++ denops/@fall-extension/source/oldfiles.ts
@@ -0,0 +1,22 @@
+import type { GetSource } from "../../@fall/source.ts";
+import * as vars from "https://deno.land/x/denops_std@v6.3.0/variable/mod.ts";
+import { ensure, is } from "jsr:@core/unknownutil@3.18.0";
+
+export const getSource: GetSource = (denops, _options) => {
+  return {
+    async stream() {
+      const oldfiles = ensure(
+        await vars.v.get(denops, "oldfiles"),
+        is.ArrayOf(is.String),
+      );
+      const items = oldfiles
+        .map((path) => ({
+          value: path,
+          detail: {
+            path,
+          },
+        }));
+      return ReadableStream.from(items);
+    },
+  };
+};
