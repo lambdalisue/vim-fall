026c5d359df34bd406c450ab3f5a6e3a7d3f13a8
Alisue <lambdalisue@gmail.com>
2024-06-01 18:06:06 +0900

    :+1: Simplify `util/input`

--- denops/@fall/util/input.ts
+++ denops/@fall/util/input.ts
@@ -1,19 +1,12 @@
 import type { Denops } from "jsr:@denops/core@6.1.0";
-import { ensure, is, type Predicate } from "jsr:@core/unknownutil@3.18.0";
+import { ensure, is } from "jsr:@core/unknownutil@3.18.0";
 
-export interface InputParams {
+export type InputParams = {
   prompt: string;
   text?: string;
   completion?: string;
   title?: string;
-}
-
-export const isInputParams = is.ObjectOf({
-  prompt: is.String,
-  text: is.OptionalOf(is.String),
-  completion: is.OptionalOf(is.String),
-  title: is.OptionalOf(is.String),
-}) satisfies Predicate<InputParams>;
+};
 
 export async function input(
   denops: Denops,
