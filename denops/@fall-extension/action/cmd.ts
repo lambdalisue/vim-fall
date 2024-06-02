b6298ed391b8841ffbdc9db1a9dc131e3fe6d43f
Alisue <lambdalisue@gmail.com>
2024-06-02 16:23:54 +0900

    :boom: Refine `cmd` action

--- denops/@fall-extension/action/cmd.ts
+++ denops/@fall-extension/action/cmd.ts
@@ -1,45 +1,71 @@
+import type { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
 import type { GetAction } from "jsr:@lambdalisue/vim-fall@0.6.0/action";
+import { input } from "https://deno.land/x/denops_std@v6.4.0/helper/input.ts";
 import { assert, is } from "jsr:@core/unknownutil@3.18.0";
 
 const description = `
-Invoke Vim command with the cursor or selected item(s).
+Invoke Vim command (item.value) with the cursor or selected item(s).
+
+# Options
+
+- immediate (boolean): Execute the command immediately without prompting the user. Default is false.
+- prefix (string): Prefix string to add before the value. Default is ''.
+- suffix (string): Suffix string to add after the value. Default is ''.
 `.trim();
 
-const DEFAULT_PLACEHOLDER = "{}";
-
 const isOptions = is.StrictOf(is.PartialOf(is.ObjectOf({
-  command: is.String,
-  placeholder: is.String,
+  immediate: is.Boolean,
+  prefix: is.String,
+  suffix: is.String,
 })));
 
 export const getAction: GetAction = (denops, options) => {
   assert(options, isOptions);
-  const placeholder = options.placeholder ?? DEFAULT_PLACEHOLDER;
-  const command = options.command ?? `echomsg '${placeholder}'`;
-  if (!command.includes(placeholder)) {
-    throw new Error(
-      `The 'command' option must contain the placeholder '${placeholder}' but got '${command}'`,
-    );
-  }
+  const immediate = options.immediate ?? false;
+  const prefix = options.prefix ?? "";
+  const suffix = options.suffix ?? "";
   return {
     description,
 
-    async invoke({ cursorItem, selectedItems }) {
+    invoke({ cursorItem, selectedItems }) {
       const items = selectedItems.length > 0
         ? selectedItems
         : cursorItem
         ? [cursorItem]
         : [];
-      for (const item of items) {
-        const cmd = command.replace(placeholder, item.value);
-        try {
-          await denops.cmd(cmd);
-        } catch (err) {
-          if (err instanceof DOMException && err.name === "AbortError") return;
-          const m = err.message ?? err;
-          console.warn(`[fall] Failed to execute '${cmd}': ${m}`);
+      const inner = async () => {
+        for (const item of items) {
+          const cmd = `${prefix}${item.value}${suffix}`;
+          try {
+            await execute(denops, cmd, immediate);
+          } catch (err) {
+            if (err instanceof DOMException && err.name === "AbortError") {
+              return;
+            }
+            const m = err.message ?? err;
+            console.warn(`[fall] Failed to execute '${cmd}': ${m}`);
+          }
         }
-      }
+      };
+      // Execute action AFTER the picker is closed
+      setTimeout(() => inner(), 0);
     },
   };
 };
+
+async function execute(
+  denops: Denops,
+  cmd: string,
+  immediate: boolean,
+): Promise<void> {
+  const command = immediate ? cmd : await input(denops, {
+    prompt: ":",
+    text: cmd,
+    completion: "command",
+  });
+  if (command == null) {
+    // Cancelled
+    return;
+  }
+  await denops.cmd(command);
+}
