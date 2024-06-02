c5163d42f8986c470f23917266defa0e3d62b0e1
Alisue <lambdalisue@gmail.com>
2024-06-02 21:34:11 +0900

    :+1: Improve `cmd` action

--- denops/@fall-extension/action/cmd.ts
+++ denops/@fall-extension/action/cmd.ts
@@ -1,31 +1,55 @@
 import type { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
 import type { GetAction } from "jsr:@lambdalisue/vim-fall@0.6.0/action";
+import * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
 import { input } from "https://deno.land/x/denops_std@v6.4.0/helper/input.ts";
-import { assert, is } from "jsr:@core/unknownutil@3.18.0";
+import { dirname } from "jsr:@std/path@0.225.0/dirname";
+import { assert, is, Predicate } from "jsr:@core/unknownutil@3.18.0";
 
-const description = `
-Invoke Vim command (item.value) with the cursor or selected item(s).
+import { retrieve } from "../util.ts";
 
-# Options
-
-- immediate (boolean): Execute the command immediately without prompting the user. Default is false.
-- prefix (string): Prefix string to add before the value. Default is ''.
-- suffix (string): Suffix string to add after the value. Default is ''.
-`.trim();
+type Restriction = "file" | "directory" | "directory-or-parent" | "buffer";
 
 const isOptions = is.StrictOf(is.PartialOf(is.ObjectOf({
+  // Attribute names to retrieve the value
+  attrs: is.ArrayOf(is.String),
+
+  // Execute the command immediately without confirmation
   immediate: is.Boolean,
+
+  // Prefix of the command
   prefix: is.String,
+
+  // Suffix of the command
   suffix: is.String,
+
+  // Restrict items that point to a file or a directory
+  restriction: is.LiteralOneOf(
+    [
+      "file",
+      "directory",
+      "directory-or-parent",
+      "buffer",
+    ] as const,
+  ) satisfies Predicate<Restriction>,
+
+  // Escape the value of the item with `fnameescape()`
+  fnameescape: is.Boolean,
+
+  // Escape the value of the item with `shellescape()`
+  shellescape: is.Boolean,
 })));
 
 export const getAction: GetAction = (denops, options) => {
   assert(options, isOptions);
+  const attrs = options.attrs ?? ["value"];
   const immediate = options.immediate ?? false;
   const prefix = options.prefix ?? "";
   const suffix = options.suffix ?? "";
+  const restriction = options.restriction;
+  const fnameescape = options.fnameescape ?? false;
+  const shellescape = options.shellescape ?? false;
   return {
-    description,
+    description: "Execute command with the cursor or selected item(s)",
 
     invoke({ cursorItem, selectedItems }) {
       const items = selectedItems.length > 0
@@ -35,7 +59,19 @@ export const getAction: GetAction = (denops, options) => {
         : [];
       const inner = async () => {
         for (const item of items) {
-          const cmd = `${prefix}${item.value}${suffix}`;
+          let value = retrieve(item, attrs, is.String);
+          if (value == undefined) continue;
+          if (restriction) {
+            value = await applyRestriction(denops, value, restriction);
+            if (value == undefined) continue;
+          }
+          if (fnameescape) {
+            value = await fn.fnameescape(denops, value);
+          }
+          if (shellescape) {
+            value = await fn.shellescape(denops, value);
+          }
+          const cmd = `${prefix}${value}${suffix}`;
           try {
             await execute(denops, cmd, immediate);
           } catch (err) {
@@ -52,6 +88,50 @@ export const getAction: GetAction = (denops, options) => {
     },
   };
 };
+
+async function applyRestriction(
+  denops: Denops,
+  value: string,
+  restriction: Restriction,
+): Promise<string | undefined> {
+  switch (restriction) {
+    case "file":
+    case "directory":
+    case "directory-or-parent": {
+      try {
+        const stat = await Deno.stat(value);
+        switch (restriction) {
+          case "file":
+            if (stat.isFile) {
+              return value;
+            }
+            break;
+          case "directory":
+            if (stat.isDirectory) {
+              return value;
+            }
+            break;
+          case "directory-or-parent":
+            if (!stat.isDirectory) {
+              value = dirname(value);
+            }
+            return value;
+        }
+      } catch (err) {
+        if (!(err instanceof Deno.errors.NotFound)) {
+          throw err;
+        }
+      }
+      return;
+    }
+    case "buffer": {
+      if (!await fn.bufloaded(denops, value)) {
+        return;
+      }
+      return value;
+    }
+  }
+}
 
 async function execute(
   denops: Denops,
