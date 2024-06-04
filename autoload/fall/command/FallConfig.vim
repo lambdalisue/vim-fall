4226fcad5c6278ee5839c90177196837c20131c4
Alisue <lambdalisue@gmail.com>
2024-06-04 21:47:54 +0900

    :+1: Add `--overwrite-with-default` option to `FallConfig`

--- autoload/fall/command/FallConfig.vim
+++ autoload/fall/command/FallConfig.vim
@@ -7,6 +7,6 @@ endfunction
 
 function! fall#command#FallConfig#complete(arglead, cmdline, cursorpos) abort
   let l:pattern = printf('\M^%s', escape(a:arglead, '^$~.*[]\'))
-  let l:sources = ['extension', 'picker', 'style']
+  let l:sources = ['extension', 'picker', 'style', '--overwrite-with-default']
   return filter(l:sources, { _, v -> v =~? l:pattern })
 endfunction
--- denops/fall/main/config.ts
+++ denops/fall/main/config.ts
@@ -1,6 +1,7 @@
 import type { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
 import * as buffer from "https://deno.land/x/denops_std@v6.4.0/buffer/mod.ts";
-import { assert, is } from "jsr:@core/unknownutil@3.18.0";
+import { parseArgs } from "jsr:@std/cli@0.224.5/parse-args";
+import { ensure, is } from "jsr:@core/unknownutil@3.18.0";
 
 import {
   getConfigDir,
@@ -9,14 +10,16 @@ import {
   loadStyleConfig,
 } from "../config/mod.ts";
 
-async function getConfigPath(type: string, configDir: string): Promise<string> {
+async function getConfigPath(type: string, configDir: string, options: {
+  overwriteWithDefault: boolean;
+}): Promise<string> {
   switch (type) {
     case "extension":
-      return (await loadExtensionConfig(configDir)).path;
+      return (await loadExtensionConfig(configDir, options)).path;
     case "picker":
-      return (await loadPickerConfig(configDir)).path;
+      return (await loadPickerConfig(configDir, options)).path;
     case "style":
-      return (await loadStyleConfig(configDir)).path;
+      return (await loadStyleConfig(configDir, options)).path;
     default:
       throw new Error(`Unknown config type: ${type}`);
   }
@@ -33,10 +36,18 @@ export function main(denops: Denops): void {
   denops.dispatcher = {
     ...denops.dispatcher,
     "config:edit": async (args) => {
-      assert(args, is.ArrayOf(is.String));
-      const type = args[0];
+      const { "overwrite-with-default": overwriteWithDefault, _: [type] } =
+        parseArgs(
+          ensure(args, is.ArrayOf(is.String)),
+          {
+            string: ["_"],
+            boolean: ["overwrite-with-default"],
+          },
+        );
       const configDir = await getConfigDir(denops);
-      const configPath = await getConfigPath(type, configDir);
+      const configPath = await getConfigPath(type, configDir, {
+        overwriteWithDefault,
+      });
       await editConfigFile(denops, configPath);
     },
   };
