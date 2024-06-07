109f770205482f0d27a60c8d310579888fadc592
Alisue <lambdalisue@gmail.com>
2024-06-07 14:59:20 +0900

    :herb: Fix test

--- denops/fall/ui/util/emitter_test.ts
+++ denops/fall/ui/util/emitter_test.ts
@@ -4,20 +4,6 @@ import { DenopsStub } from "https://deno.land/x/denops_test@v1.6.2/mod.ts";
 import { emitPickerEnter, emitPickerLeave } from "./emitter.ts";
 
 Deno.test("emitPickerEnter", async (t) => {
-  await t.step("invoke 'fall#internal#mapping#store'", async () => {
-    const called: [string, unknown[]][] = [];
-    const denops = new DenopsStub({
-      call(name, ...args): Promise<unknown> {
-        called.push([name, args]);
-        return Promise.resolve();
-      },
-    });
-    await emitPickerEnter(denops, "test");
-    assertEquals(called, [
-      ["fall#internal#mapping#store", []],
-    ]);
-  });
-
   await t.step("emit 'User FallPickerEnter:{name}'", async () => {
     const called: [string, Record<PropertyKey, unknown>][] = [];
     const denops = new DenopsStub({
@@ -34,20 +20,6 @@ Deno.test("emitPickerEnter", async (t) => {
 });
 
 Deno.test("emitPickerLeave", async (t) => {
-  await t.step("invoke 'fall#internal#mapping#restore'", async () => {
-    const called: [string, unknown[]][] = [];
-    const denops = new DenopsStub({
-      call(name, ...args): Promise<unknown> {
-        called.push([name, args]);
-        return Promise.resolve();
-      },
-    });
-    await emitPickerLeave(denops, "test");
-    assertEquals(called, [
-      ["fall#internal#mapping#restore", []],
-    ]);
-  });
-
   await t.step("emit 'User FallPickerLeave:{name}'", async () => {
     const called: [string, Record<PropertyKey, unknown>][] = [];
     const denops = new DenopsStub({
