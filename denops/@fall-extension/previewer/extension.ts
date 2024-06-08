4ddc0e704f94ebd65e4913ed2996777fd553a1cf
Alisue <lambdalisue@gmail.com>
2024-06-08 10:01:34 +0900

    :+1: Improve `extension` preview

--- denops/@fall-extension/previewer/extension.ts
+++ denops/@fall-extension/previewer/extension.ts
@@ -1,35 +1,45 @@
 import type { GetPreviewer } from "jsr:@lambdalisue/vim-fall@0.6.0/previewer";
+import { stringify } from "jsr:@std/yaml@0.224.1/stringify";
+import { basename } from "jsr:@std/path@1.0.0-rc.1/basename";
+import { is } from "jsr:@core/unknownutil@3.18.0";
 
-import { getPreviewer as getAttrPreviewer } from "./attr.ts";
-import { getPreviewer as getFilePreviewer } from "./file.ts";
+const isExtensionDetail = is.ObjectOf({
+  extension: is.ObjectOf({
+    type: is.String,
+    name: is.String,
+    script: is.String,
+    config: is.OptionalOf(is.Record),
+  }),
+});
 
-export const getPreviewer: GetPreviewer = async (denops, options) => {
-  const attrPreviewer = await getAttrPreviewer(denops, options);
-  const filePreviewer = await getFilePreviewer(denops, options);
+export const getPreviewer: GetPreviewer = () => {
   return {
-    async preview(params, options) {
-      const attrPreview = await attrPreviewer.preview(params, options) ?? {
-        content: [],
-      };
-      const filePreview = await filePreviewer.preview(params, options) ?? {
-        content: [],
-      };
+    async preview({ item }, { signal }) {
+      if (!isExtensionDetail(item.detail)) {
+        return;
+      }
+      const { type, name, script, config } = item.detail.extension;
+      const text = splitText(await Deno.readTextFile(script));
+      signal?.throwIfAborted();
+      const yaml = splitText(stringify({
+        [type]: {
+          [name]: config ?? {},
+        },
+      }));
+      const content = [
+        `// # Applied configuration`,
+        ...yaml.map((line) => `// ${line}`),
+        ...text,
+      ];
       return {
-        filetype: "markdown",
-        content: [
-          "# Options",
-          "```json",
-          ...attrPreview.content,
-          "```",
-          "",
-          "# Source code",
-          "```typescript",
-          ...filePreview.content,
-          "```",
-        ],
-        line: attrPreview.content.length + (filePreview.line ?? 0),
-        column: attrPreview.column ?? 1,
+        filename: basename(script),
+        content,
       };
     },
   };
 };
+
+function splitText(text: string): string[] {
+  const lines = text.split(/\r?\n/g);
+  return lines.at(-1) === "" ? lines.slice(0, -1) : lines;
+}
