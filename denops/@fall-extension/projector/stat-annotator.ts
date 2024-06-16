2d999f90788d565c0bc2d10006e91b2def8a1608
Alisue <lambdalisue@gmail.com>
2024-06-16 15:05:03 +0900

    :+1: Add `stat-annotator` projector

---
+++ denops/@fall-extension/projector/stat-annotator.ts
@@ -0,0 +1,38 @@
+import type { GetProjector } from "jsr:@lambdalisue/vim-fall@0.6.0/projector";
+import { is } from "jsr:@core/unknownutil@3.18.0";
+
+const isPathDetail = is.ObjectOf({
+  path: is.String,
+  stat: is.OptionalOf(is.Record),
+});
+
+export const getProjector: GetProjector = (_denops, _options) => {
+  return {
+    async project({ items }, { signal }) {
+      return await Promise.all(items.map(async (item) => {
+        signal?.throwIfAborted();
+        if (!isPathDetail(item.detail)) {
+          return item;
+        }
+        if (item.detail.stat) {
+          return item;
+        }
+        try {
+          const stat = await Deno.stat(item.detail.path);
+          return {
+            ...item,
+            detail: {
+              ...item.detail,
+              stat,
+            },
+          };
+        } catch (err) {
+          if (err instanceof Deno.errors.NotFound) {
+            return item;
+          }
+          throw err;
+        }
+      }));
+    },
+  };
+};
