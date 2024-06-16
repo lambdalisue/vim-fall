b562d6a1ab78bfa79ab042f8e79dcd14b1441a13
Alisue <lambdalisue@gmail.com>
2024-06-16 14:42:01 +0900

    :+1: Improve `lexical-sorter` to support arbitrary attribute

--- denops/@fall-extension/projector/lexical-sorter.ts
+++ denops/@fall-extension/projector/lexical-sorter.ts
@@ -1,18 +1,24 @@
 import type { GetProjector } from "jsr:@lambdalisue/vim-fall@0.6.0/projector";
 import { assert, is } from "jsr:@core/unknownutil@3.18.0";
 
+import { retrieve } from "../util.ts";
+
 const isOptions = is.StrictOf(is.PartialOf(is.ObjectOf({
+  attrs: is.ArrayOf(is.String),
   reverse: is.Boolean,
 })));
 
 export const getProjector: GetProjector = (_denops, options) => {
   assert(options, isOptions);
+  const attrs = options.attrs ?? ["value"];
   const alpha = options.reverse ? -1 : 1;
   return {
     project({ items }, { signal }) {
       return items.toSorted((a, b) => {
         signal?.throwIfAborted();
-        return a.value.localeCompare(b.value) * alpha;
+        const va = retrieve(a, attrs, is.String) ?? "";
+        const vb = retrieve(b, attrs, is.String) ?? "";
+        return va.localeCompare(vb) * alpha;
       });
     },
   };
