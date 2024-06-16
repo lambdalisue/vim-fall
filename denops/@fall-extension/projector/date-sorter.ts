1fcc0084c4fc02f21597a86ea91b0d1a36f3b9b3
Alisue <lambdalisue@gmail.com>
2024-06-16 15:05:44 +0900

    :+1: Add `date-sorter` projector

---
+++ denops/@fall-extension/projector/date-sorter.ts
@@ -0,0 +1,28 @@
+import type { GetProjector } from "jsr:@lambdalisue/vim-fall@0.6.0/projector";
+import { assert, is } from "jsr:@core/unknownutil@3.18.0";
+
+import { retrieve } from "../util.ts";
+
+const isOptions = is.StrictOf(is.PartialOf(is.ObjectOf({
+  attrs: is.ArrayOf(is.String),
+  reverse: is.Boolean,
+})));
+
+const isDate = is.InstanceOf(Date);
+
+export const getProjector: GetProjector = (_denops, options) => {
+  assert(options, isOptions);
+  const attrs = options.attrs ?? ["value"];
+  const alpha = options.reverse ? -1 : 1;
+  return {
+    project({ items }, { signal }) {
+      return items.toSorted((a, b) => {
+        signal?.throwIfAborted();
+        const ta = retrieve(a, attrs, isDate);
+        const tb = retrieve(b, attrs, isDate);
+        if (!ta || !tb) return 0;
+        return Math.sign(tb.getTime() - ta.getTime()) * alpha;
+      });
+    },
+  };
+};
