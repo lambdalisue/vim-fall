f1a6845e958aa18a152ad88d84f6e76dbfd1f3c8
Alisue <lambdalisue@gmail.com>
2024-11-16 16:59:23 +0900

    feat: simplify event dispatching

--- denops/fall/main/event.ts
+++ denops/fall/main/event.ts
@@ -1,19 +1,101 @@
 import type { Entrypoint } from "jsr:@denops/std@^7.3.2";
-import { ensure } from "jsr:@core/unknownutil@^4.3.0";
+import { as, ensure, is, type Predicate } from "jsr:@core/unknownutil@^4.3.0";
 
-import { dispatch } from "../event.ts";
-import { isEventComplement } from "../util/predicate.ts";
+import { dispatch, type Event } from "../event.ts";
 
 export const main: Entrypoint = (denops) => {
   denops.dispatcher = {
     ...denops.dispatcher,
     "event:dispatch": (event) => {
-      return eventDispatch(event);
+      dispatch(ensure(event, isEventComplement));
     },
   };
-  return Promise.resolve();
 };
 
-function eventDispatch(event: unknown): void {
-  dispatch(ensure(event, isEventComplement));
-}
+const isSelectMethod = is.LiteralOneOf(["on", "off", "toggle"] as const);
+
+// NOTE:
+// Only event that is dispatched from Vim requires predicate function.
+// So this is NOT a full list of event types.
+export const isEventComplement = is.UnionOf([
+  // Cursor
+  is.ObjectOf({
+    type: is.LiteralOf("move-cursor"),
+    amount: is.Number,
+    scroll: as.Optional(is.Boolean),
+  }),
+  is.ObjectOf({
+    type: is.LiteralOf("move-cursor-at"),
+    cursor: is.UnionOf([is.Number, is.LiteralOf("$")]),
+  }),
+  // Select
+  is.ObjectOf({
+    type: is.LiteralOf("select-item"),
+    cursor: as.Optional(is.UnionOf([is.Number, is.LiteralOf("$")])),
+    method: as.Optional(isSelectMethod),
+  }),
+  is.ObjectOf({
+    type: is.LiteralOf("select-all-items"),
+    method: as.Optional(isSelectMethod),
+  }),
+  // Switch
+  is.ObjectOf({
+    type: is.LiteralOf("switch-matcher"),
+    amount: is.Number,
+    cycle: as.Optional(is.Boolean),
+  }),
+  is.ObjectOf({
+    type: is.LiteralOf("switch-matcher-at"),
+    index: is.UnionOf([is.Number, is.LiteralOf("$")]),
+  }),
+  is.ObjectOf({
+    type: is.LiteralOf("switch-sorter"),
+    amount: is.Number,
+    cycle: as.Optional(is.Boolean),
+  }),
+  is.ObjectOf({
+    type: is.LiteralOf("switch-sorter-at"),
+    index: is.UnionOf([is.Number, is.LiteralOf("$")]),
+  }),
+  is.ObjectOf({
+    type: is.LiteralOf("switch-renderer"),
+    amount: is.Number,
+    cycle: as.Optional(is.Boolean),
+  }),
+  is.ObjectOf({
+    type: is.LiteralOf("switch-renderer-at"),
+    index: is.UnionOf([is.Number, is.LiteralOf("$")]),
+  }),
+  is.ObjectOf({
+    type: is.LiteralOf("switch-previewer"),
+    amount: is.Number,
+    cycle: as.Optional(is.Boolean),
+  }),
+  is.ObjectOf({
+    type: is.LiteralOf("switch-previewer-at"),
+    index: is.UnionOf([is.Number, is.LiteralOf("$")]),
+  }),
+  // Action
+  is.ObjectOf({
+    type: is.LiteralOf("action-invoke"),
+    name: is.String,
+  }),
+  // List
+  is.ObjectOf({
+    type: is.LiteralOf("list-component-execute"),
+    command: is.String,
+  }),
+  // Preview
+  is.ObjectOf({
+    type: is.LiteralOf("preview-component-execute"),
+    command: is.String,
+  }),
+  // Help
+  is.ObjectOf({
+    type: is.LiteralOf("help-component-toggle"),
+  }),
+  is.ObjectOf({
+    type: is.LiteralOf("help-component-page"),
+    amount: is.Number,
+  }),
+]) satisfies Predicate<Event>;
--- denops/fall/util/predicate.ts
+++ denops/fall/util/predicate.ts
@@ -14,7 +14,6 @@ import type {
 } from "jsr:@vim-fall/core@^0.2.1";
 
 import type { GlobalConfig, ItemPickerParams } from "../config.ts";
-import type { Event } from "../event.ts";
 
 export const isStringArray = is.ArrayOf(is.String);
 
@@ -106,91 +105,3 @@ export const isOptions = is.ObjectOf({
 export function isIncrementalMatcher<T extends Detail>(m: Matcher<T>): boolean {
   return (m as { incremental?: boolean }).incremental ?? false;
 }
-
-const isSelectMethod = is.LiteralOneOf(["on", "off", "toggle"] as const);
-
-// NOTE:
-// Only event that is dispatched from Vim requires predicate function.
-// So this is NOT a full list of event types.
-export const isEventComplement = is.UnionOf([
-  // Cursor
-  is.ObjectOf({
-    type: is.LiteralOf("move-cursor"),
-    amount: is.Number,
-    scroll: as.Optional(is.Boolean),
-  }),
-  is.ObjectOf({
-    type: is.LiteralOf("move-cursor-at"),
-    cursor: is.UnionOf([is.Number, is.LiteralOf("$")]),
-  }),
-  // Select
-  is.ObjectOf({
-    type: is.LiteralOf("select-item"),
-    cursor: as.Optional(is.UnionOf([is.Number, is.LiteralOf("$")])),
-    method: as.Optional(isSelectMethod),
-  }),
-  is.ObjectOf({
-    type: is.LiteralOf("select-all-items"),
-    method: as.Optional(isSelectMethod),
-  }),
-  // Switch
-  is.ObjectOf({
-    type: is.LiteralOf("switch-matcher"),
-    amount: is.Number,
-    cycle: as.Optional(is.Boolean),
-  }),
-  is.ObjectOf({
-    type: is.LiteralOf("switch-matcher-at"),
-    index: is.UnionOf([is.Number, is.LiteralOf("$")]),
-  }),
-  is.ObjectOf({
-    type: is.LiteralOf("switch-sorter"),
-    amount: is.Number,
-    cycle: as.Optional(is.Boolean),
-  }),
-  is.ObjectOf({
-    type: is.LiteralOf("switch-sorter-at"),
-    index: is.UnionOf([is.Number, is.LiteralOf("$")]),
-  }),
-  is.ObjectOf({
-    type: is.LiteralOf("switch-renderer"),
-    amount: is.Number,
-    cycle: as.Optional(is.Boolean),
-  }),
-  is.ObjectOf({
-    type: is.LiteralOf("switch-renderer-at"),
-    index: is.UnionOf([is.Number, is.LiteralOf("$")]),
-  }),
-  is.ObjectOf({
-    type: is.LiteralOf("switch-previewer"),
-    amount: is.Number,
-    cycle: as.Optional(is.Boolean),
-  }),
-  is.ObjectOf({
-    type: is.LiteralOf("switch-previewer-at"),
-    index: is.UnionOf([is.Number, is.LiteralOf("$")]),
-  }),
-  // Action
-  is.ObjectOf({
-    type: is.LiteralOf("action-invoke"),
-    name: is.String,
-  }),
-  // List
-  is.ObjectOf({
-    type: is.LiteralOf("list-component-execute"),
-    command: is.String,
-  }),
-  // Preview
-  is.ObjectOf({
-    type: is.LiteralOf("preview-component-execute"),
-    command: is.String,
-  }),
-  // Help
-  is.ObjectOf({
-    type: is.LiteralOf("help-component-toggle"),
-  }),
-  is.ObjectOf({
-    type: is.LiteralOf("help-component-page"),
-    amount: is.Number,
-  }),
-]) satisfies Predicate<Event>;
